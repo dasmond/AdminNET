a86b4f948ea3dbf1961decb80a0ec5b914c77190
jinchangkai <619578408@qq.com>
2021-04-20 08:35:24 +0800

    11111

---
+++ backend/Dilon.Application/Service/CodeGenTest/ICodeGenTestService.cs
@@ -0,0 +1,16 @@
+﻿using Dilon.Core;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace Dilon.Application
+{
+    public interface ICodeGenTestService
+    {
+        Task Add(AddCodeGenTestInput input);
+        Task Delete(DeleteCodeGenTestInput input);
+        Task<CodeGenTest> Get([FromQuery] QueryeCodeGenTestInput input);
+        Task<dynamic> List([FromQuery] CodeGenTestInput input);
+        Task<dynamic> Page([FromQuery] CodeGenTestInput input);
+        Task Update(UpdateCodeGenTestInput input);
+    }
+}
---
+++ frontend/src/api/modular/main/CodeGenTestManage.js
@@ -0,0 +1,66 @@
+﻿import { axios } from '@/utils/request'
+
+/**
+ * 查询代码生成业务
+ *
+ * @author Jck
+ */
+export function CodeGenTestPage (parameter) {
+  return axios({
+    url: '/CodeGenTest/page',
+    method: 'get',
+    params: parameter
+  })
+}
+
+/**
+ * 代码生成业务列表
+ *
+ * @author Jck
+ */
+export function CodeGenTestList (parameter) {
+  return axios({
+    url: '/CodeGenTest/list',
+    method: 'get',
+    params: parameter
+  })
+}
+
+/**
+ * 添加代码生成业务
+ *
+ * @author Jck
+ */
+export function CodeGenTestAdd (parameter) {
+  return axios({
+    url: '/CodeGenTest/add',
+    method: 'post',
+    data: parameter
+  })
+}
+
+/**
+ * 编辑代码生成业务
+ *
+ * @author Jck
+ */
+export function CodeGenTestEdit (parameter) {
+  return axios({
+    url: '/CodeGenTest/edit',
+    method: 'post',
+    data: parameter
+  })
+}
+
+/**
+ * 删除代码生成业务
+ *
+ * @author Jck
+ */
+export function CodeGenTestDelete (parameter) {
+  return axios({
+    url: '/CodeGenTest/delete',
+    method: 'post',
+    data: parameter
+  })
+}
