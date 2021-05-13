dd4ded5271595d3ed91d6112a5c8564763e2f5d6
Wogoo <58164020@qq.com>
2021-05-13 14:33:05 +0800

    测试 fork

---
+++ backend/Admin.NET.Application/Entity/Terminal.cs
@@ -0,0 +1,18 @@
+﻿using Admin.NET.Core;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Admin.NET.Application
+{
+    public class Terminal: DEntityBase
+    {
+        [Comment("账号")]
+        [Required, MaxLength(20)]
+        public string ShowName { get; set; }
+    }
+}
---
+++ backend/Admin.NET.Application/Service/TerminalAppService.cs
@@ -0,0 +1,22 @@
+﻿using Furion.DynamicApiController;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Admin.NET.Application.Service
+{
+    //[ApiDescriptionSettings("自己的业务", Name = "Test", Order = 100)]
+    [ApiDescriptionSettings("自己的业务")]
+    public class TerminalAppService : IDynamicApiController
+    {
+        [AllowAnonymous]
+        public string ShowName() 
+        {
+            return "ShowName";
+        }
+    }
+}
