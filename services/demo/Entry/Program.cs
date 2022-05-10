546baaafab24fa8191b1b7cc7af78267cc19d7de
sanmen359 <sanmen359@hotmail.com>
2022-05-10 19:45:36 +0800

    Seq地址改成从环境变量中获取

--- services/demo/Entry/Program.cs
+++ services/demo/Entry/Program.cs
@@ -1,5 +1,10 @@
+using Serilog;
 var builder = WebApplication.CreateBuilder(args).Inject();
-builder.Host.UseSerilogDefault();
-
+builder.Host.UseSerilogDefault(config =>
+{
+    //读取环境变量
+    var seqServerUrl = builder.Configuration["SeqServerUrl"];
+    config.WriteTo.Seq(seqServerUrl ?? "http://localhost:5380");
+});
 var app = builder.Build();
 app.Run();
