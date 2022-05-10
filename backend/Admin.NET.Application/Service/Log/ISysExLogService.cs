5ff3b53fc0af3027abf62e82d5d7ea7d1cb18f1b
Jerry <chenjingchun@foxmail.com>
2022-05-10 14:06:50 +0800

    恢复log服务

---
+++ backend/Admin.NET.Application/Service/Log/ISysExLogService.cs
@@ -0,0 +1,12 @@
+﻿using Admin.NET.Core;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Admin.NET.Application
+{
+    public interface ISysExLogService
+    {
+        Task ClearExLog();
+
+        Task<PageResult<ExLogOutput>> QueryExLogPageList([FromQuery] ExLogPageInput input);
+    }
+}
---
+++ backend/Admin.NET.Application/Service/Log/ISysOpLogService.cs
@@ -0,0 +1,12 @@
+﻿using Admin.NET.Core;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Admin.NET.Application
+{
+    public interface ISysOpLogService
+    {
+        Task ClearOpLog();
+
+        Task<PageResult<OpLogOutput>> QueryOpLogPageList([FromQuery] OpLogPageInput input);
+    }
+}
