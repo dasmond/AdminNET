76c323f782a77e426853f147f33898a271df6c7f
xingzhe <xingzhe@yuncreator.com>
2023-05-04 18:38:03 +0800

    添加log的字段；

--- Admin.NET/Admin.NET.Application/Entity/Log.cs
+++ Admin.NET/Admin.NET.Application/Entity/Log.cs
@@ -9,5 +9,9 @@ public class Log : EntityBaseId
     [SugarColumn(ColumnDescription = "人脸Id", Length = 256)]
     public string? FaceId { get; set; }
 
+    [SugarColumn(ColumnDescription = "图片", Length = 512)]
+    public string? FaceUrl { get; set; }
 
+    [SugarColumn(ColumnDescription = "创建时间", IsOnlyIgnoreUpdate = true)]
+    public virtual DateTime? CreateTime { get; set; }
 }
