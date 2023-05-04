dcb33ab0c65907156a4c14f409386e2d26052ba9
xingzhe <xingzhe@yuncreator.com>
2023-05-04 18:29:30 +0800

    添加视频合成的日志；

---
+++ Admin.NET/Admin.NET.Application/Entity/Log.cs
@@ -0,0 +1,13 @@
+﻿namespace Admin.NET.Application.Entity;
+
+[SugarTable("VcLog", "视频合成的日志")]
+public class Log : EntityBaseId
+{
+    [SugarColumn(ColumnDescription = "类型")]
+    public LogType Type { get; set; }
+
+    [SugarColumn(ColumnDescription = "人脸Id", Length = 256)]
+    public string? FaceId { get; set; }
+
+
+}
