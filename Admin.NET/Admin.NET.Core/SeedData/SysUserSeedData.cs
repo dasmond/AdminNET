b435cc124aa54cf7a13dac77ace0048becfa4d8f
xuzhenlei <xuzhenlei@hotmail.com>
2023-05-07 15:59:13 +0800

    修改超级管理员账号为super

--- Admin.NET/Admin.NET.Core/SeedData/SysUserSeedData.cs
+++ Admin.NET/Admin.NET.Core/SeedData/SysUserSeedData.cs
@@ -16,8 +16,8 @@ public class SysUserSeedData : ISqlSugarEntitySeedData<SysUser>
 
         return new[]
         {
-            new SysUser{ Id=1300000000101, Account="superadmin", Password=encryptPasswod, NickName="超级管理员", RealName="超级管理员", Phone="18020030720", Birthday=DateTime.Parse("1986-06-28"), Sex=GenderEnum.Male, AccountType=AccountTypeEnum.SuperAdmin, Remark="超级管理员", CreateTime=DateTime.Parse("2022-02-10 00:00:00"), TenantId=1300000000001 },
-            new SysUser{ Id=1300000000111, Account="admin", Password=encryptPasswod, NickName="系统管理员", RealName="系统管理员", Phone="18020030720", Birthday=DateTime.Parse("1986-06-28"), Sex=GenderEnum.Male, AccountType=AccountTypeEnum.Admin, Remark="系统管理员", CreateTime=DateTime.Parse("2022-02-10 00:00:00"), OrgId=1300000000101, PosId=1310000000001, TenantId=1300000000001 },
+            new SysUser{ Id=1300000000101, Account="super", Password=encryptPasswod, NickName="超级管理员", RealName="超级管理员", Phone="18020030720", Birthday=DateTime.Parse("1986-06-28"), Sex=GenderEnum.Male, AccountType=AccountTypeEnum.SuperAdmin, Remark="超级管理员", CreateTime=DateTime.Parse("2022-02-10 00:00:00"), TenantId=1300000000001 },
+            new SysUser{ Id=1300000000111, Account="admin", Password=encryptPasswod, NickName="系统管理员", RealName="系统管理员", Phone="18020030720", Birthday=DateTime.Parse("1986-06-28"), Sex=GenderEnum.Male, AccountType=AccountTypeEnum.Admin, Remark="系统管理员", CreateTime=DateTime.Parse("2022-02-10 00:00:00"),TenantId=1300000000001, OrgId=1300000000101, PosId=1310000000001 },
         };
     }
 }
