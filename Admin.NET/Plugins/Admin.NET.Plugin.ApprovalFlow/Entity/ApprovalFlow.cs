541f110a0f725b419582f5fef89961b8cdadf3b5
弹剑听潮 <andyaicao@163.com>
2024-05-07 01:53:18 +0000

    更新表单json字段长度为长字符串
    Signed-off-by: 弹剑听潮 <andyaicao@163.com>

--- Admin.NET/Plugins/Admin.NET.Plugin.ApprovalFlow/Entity/ApprovalFlow.cs
+++ Admin.NET/Plugins/Admin.NET.Plugin.ApprovalFlow/Entity/ApprovalFlow.cs
@@ -1,4 +1,4 @@
-﻿// Admin.NET 项目的版权、商标、专利和其他相关权利均受相应法律法规的保护。使用本项目应遵守相关法律法规和许可证的要求。
+// Admin.NET 项目的版权、商标、专利和其他相关权利均受相应法律法规的保护。使用本项目应遵守相关法律法规和许可证的要求。
 //
 // 本项目主要遵循 MIT 许可证和 Apache 许可证（版本 2.0）进行分发和使用。许可证位于源代码树根目录中的 LICENSE-MIT 和 LICENSE-APACHE 文件。
 //
@@ -27,7 +27,7 @@ public class ApprovalFlow : EntityBaseData
     /// <summary>
     /// 表单
     /// </summary>
-    [SugarColumn(ColumnName = "FormJson", ColumnDescription = "表单", Length = 0)]
+    [SugarColumn(ColumnName = "FormJson", ColumnDescription = "表单", ColumnDataType = StaticConfig.CodeFirst_BigString)]
     public string? FormJson { get; set; }
 
     /// <summary>
