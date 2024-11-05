61d23ca0c7bb13ca911cb5b6a6cead5c3c70773d
zuohuaijun <zuohuaijun@163.com>
2024-11-05 21:50:50 +0800

    😎1、优化文件上传流程  2、修复通知公告逻辑错误  3、升级依赖

--- Admin.NET/Admin.NET.Core/Option/UploadOptions.cs
+++ Admin.NET/Admin.NET.Core/Option/UploadOptions.cs
@@ -43,11 +43,18 @@ public sealed class OSSProviderOptions : OSSOptions, IConfigurableOptions
     /// <summary>
     /// 是否启用OSS存储
     /// </summary>
-    public bool IsEnable { get; set; }
+    public bool Enabled { get; set; }
 
     /// <summary>
     /// 自定义桶名称 不能直接使用Provider来替代桶名称
     /// 例：阿里云 1.只能包括小写字母，数字，短横线（-）2.必须以小写字母或者数字开头 3.长度必须在3-63字节之间
     /// </summary>
     public string Bucket { get; set; }
+
+    /// <summary>
+    /// 自定义Host
+    /// 拼接外链的Host，若空则使用Endpoint拼接
+    /// </summary>
+    /// <remarks></remarks>
+    public string CustomHost { get; set; }
 }
--- Admin.NET/Admin.NET.Core/Service/File/Dto/FileInput.cs
+++ Admin.NET/Admin.NET.Core/Service/File/Dto/FileInput.cs
@@ -6,51 +6,20 @@
 
 namespace Admin.NET.Core.Service;
 
-public class FileInput : BaseIdInput
-{
-    /// <summary>
-    /// 文件名称
-    /// </summary>
-    public string FileName { get; set; }
-
-    /// <summary>
-    /// 文件类型
-    /// </summary>
-    public string FileType { get; set; }
-
-    /// <summary>
-    /// 是否公开
-    /// 若为true则所有人都可以查看，默认只有自己或有权限的可以查看
-    /// </summary>
-    public bool IsPublic { get; set; }
-
-    /// <summary>
-    /// 文件Url
-    /// </summary>
-    public string? Url { get; set; }
-
-    /// <summary>
-    /// 所属实体ID
-    /// </summary>
-    public long BelongId { get; set; }
-
-    /// <summary>
-    /// 关联对象Id
-    /// </summary>
-    public long RelationId { get; set; }
-
-    /// <summary>
-    /// 关联对象名称
-    /// </summary>
-    public string RelationName { get; set; }
-}
-
+/// <summary>
+/// 文件分页查询
+/// </summary>
 public class PageFileInput : BasePageInput
 {
     /// <summary>
     /// 文件名称
     /// </summary>
     public string FileName { get; set; }
+
+    /// <summary>
+    /// 文件后缀
+    /// </summary>
+    public string? Suffix { get; set; }
 
     /// <summary>
     /// 开始时间
@@ -63,11 +32,45 @@ public class PageFileInput : BasePageInput
     public DateTime? EndTime { get; set; }
 }
 
+public class FileInput : BaseIdInput
+{
+}
+
 public class DeleteFileInput : BaseIdInput
 {
 }
 
-public class UploadFileFromBase64Input
+/// <summary>
+/// 上传文件
+/// </summary>
+public class UploadFileInput : SysFile
+{
+    /// <summary>
+    /// 文件
+    /// </summary>
+    [Required]
+    public IFormFile File { get; set; }
+
+    /// <summary>
+    /// 文件路径
+    /// </summary>
+    public string Path { get; set; }
+
+    /// <summary>
+    /// 文件保存路径
+    /// </summary>
+    public string SavePath { get; set; }
+
+    /// <summary>
+    /// 允许格式：.jpeg.jpg.png.bmp.gif.tif
+    /// </summary>
+    public string AllowSuffix { get; set; }
+}
+
+/// <summary>
+/// 上传文件Base64
+/// </summary>
+public class UploadFileFromBase64Input : SysFile
 {
     /// <summary>
     /// 文件内容
@@ -80,83 +83,9 @@ public class UploadFileFromBase64Input
     public string ContentType { get; set; }
 
     /// <summary>
-    /// 文件名称
-    /// </summary>
-    public string FileName { get; set; }
-
-    /// <summary>
     /// 保存路径
     /// </summary>
     public string Path { get; set; }
-
-    /// <summary>
-    /// 文件类型
-    /// </summary>
-    public string FileType { get; set; }
-
-    /// <summary>
-    /// 是否公开
-    /// 若为true则所有人都可以查看，默认只有自己或有权限的可以查看
-    /// </summary>
-    public bool IsPublic { get; set; }
-
-    /// <summary>
-    /// 所属实体ID
-    /// </summary>
-    public long BelongId { get; set; }
-
-    /// <summary>
-    /// 关联对象Id
-    /// </summary>
-    public long RelationId { get; set; }
-
-    /// <summary>
-    /// 关联对象名称
-    /// </summary>
-    public string RelationName { get; set; }
-}
-
-/// <summary>
-/// 上传文件
-/// </summary>
-public class FileUploadInput
-{
-    /// <summary>
-    /// 文件
-    /// </summary>
-    [Required]
-    public IFormFile File { get; set; }
-
-    /// <summary>
-    /// 文件类型
-    /// </summary>
-    public string FileType { get; set; }
-
-    /// <summary>
-    /// 是否公开
-    /// 若为true则所有人都可以查看，默认只有自己或有权限的可以查看
-    /// </summary>
-    public bool IsPublic { get; set; }
-
-    /// <summary>
-    /// 文件路径
-    /// </summary>
-    public string Path { get; set; }
-
-    /// <summary>
-    /// 所属实体ID
-    /// </summary>
-    public long BelongId { get; set; }
-
-    /// <summary>
-    /// 关联对象Id
-    /// </summary>
-    public long RelationId { get; set; }
-
-    /// <summary>
-    /// 关联对象名称
-    /// </summary>
-    public string RelationName { get; set; }
 }
 
 /// <summary>
@@ -175,7 +104,7 @@ public class RelationQueryInput
     public long? RelationId { get; set; }
 
     /// <summary>
-    /// 文件，多个以","分割
+    /// 文件类型：多个以","分割
     /// </summary>
     public string FileTypes { get; set; }
 
@@ -185,80 +114,11 @@ public class RelationQueryInput
     public long? BelongId { get; set; }
 
     /// <summary>
-    ///
+    /// 文件类型分割
     /// </summary>
     /// <returns></returns>
     public string[] GetFileTypeBS()
     {
         return FileTypes.Split(',');
     }
-}
-
-public class FileOutput
-{
-    /// <summary>
-    /// Id
-    /// </summary>
-    public long Id { get; set; }
-
-    /// <summary>
-    /// 名称
-    /// </summary>
-    public string Name { get; set; }
-
-    /// <summary>
-    /// URL
-    /// </summary>
-    public string Url { get; set; }
-
-    /// <summary>
-    /// 大小
-    /// </summary>
-    public long SizeKb { get; set; }
-
-    /// <summary>
-    /// 后缀
-    /// </summary>
-    public string Suffix { get; set; }
-
-    /// <summary>
-    /// 路径
-    /// </summary>
-    public string FilePath { get; set; }
-
-    /// <summary>
-    /// 文件类别
-    /// </summary>
-    public string FileType { get; set; }
-
-    /// <summary>
-    /// 是否公开
-    /// 若为true则所有人都可以查看，默认只有自己或有权限的可以查看
-    /// </summary>
-    public bool IsPublic { get; set; }
-
-    /// <summary>
-    /// 上传人
-    /// </summary>
-    public string CreateUserName { get; set; }
-
-    /// <summary>
-    /// 上传时间
-    /// </summary>
-    public DateTime? CreateTime { get; set; }
-
-    /// <summary>
-    /// 关联对象名称
-    /// </summary>
-    public string RelationName { get; set; }
-
-    /// <summary>
-    /// 关联对象Id
-    /// </summary>
-    public long? RelationId { get; set; }
-
-    /// <summary>
-    /// 所属Id
-    /// </summary>
-    public long? BelongId { get; set; }
 }
