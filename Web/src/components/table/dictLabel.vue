a44460fd4d408929e322382025c71b0a68fadf2c
喵你个旺呀 <jason-dom@qq.com>
2024-11-06 21:22:15 +0800

    feat: 新增字典渲染组件

---
+++ Web/src/components/table/dictLabel.vue
@@ -0,0 +1,17 @@
+<template>
+	<ElTag :type="dict?.tagType ?? ''">{{ dict?.name ?? defaultValue ?? '-' }}</ElTag>
+</template>
+
+<script lang="ts" setup>
+import { useUserInfo } from '/@/stores/userInfo';
+import { reactive } from 'vue';
+import { ElTag } from 'element-plus';
+
+const props = defineProps<{
+	code: string;
+	value: any;
+	defaultValue: string
+}>();
+
+const dict = reactive(useUserInfo().getDictLabelByVal(props.code, props.value));
+</script>
