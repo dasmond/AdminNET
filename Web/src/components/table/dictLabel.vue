1b40d9170f0abcebcbb91531f4a44de5bdf2b050
喵你个旺呀 <jason-dom@qq.com>
2024-11-15 08:45:41 +0800

    chore: 修正枚举转字典与标准字典数据不一致问题

--- Web/src/components/table/dictLabel.vue
+++ Web/src/components/table/dictLabel.vue
@@ -5,18 +5,17 @@
 <script lang="ts" setup>
 import { useUserInfo } from '/@/stores/userInfo';
 import { reactive, onMounted } from 'vue';
-import { ElTag } from 'element-plus';
 
 const props = defineProps({
 	code: String,
 	value: Object,
 	propLabel: {
 		type: String,
-		default: 'name'
+		default: 'value'
 	},
 	propValue: {
 		type: String,
-		default: 'value'
+		default: 'code'
 	},
 	defaultValue: {
 		type: String,
@@ -26,12 +25,12 @@ const props = defineProps({
 
 const state = reactive({
   label: '' as string,
-	tagType: '' as string
+  tagType: '' as string
 });
 
 onMounted(() => {
-	const dictList = useUserInfo().getDictDatasByCode(props.code);
-	const dict = dictList?.find(x => x[props.propValue] == props.value + "") ?? {};
+	const dictList = useUserInfo().getDictDatasByCode(props.code as string);
+	const dict = dictList?.find((x: any) => x[props.propValue] == props.value + "") ?? {};
 	if (dict) {
 		state.label = dict[props.propLabel] || props.defaultValue;
 		state.tagType = dict.tagType;
