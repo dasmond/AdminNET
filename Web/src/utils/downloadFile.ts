6a2b704181dd5c5402e15d2c3338ed584d91cd1b
喵你个旺呀 <jason-dom@qq.com>
2024-11-19 00:31:36 +0800

    feat: 😀新增导出功能

--- Web/src/utils/downloadFile.ts
+++ Web/src/utils/downloadFile.ts
@@ -17,3 +17,15 @@ export function downloadFile(url: string, filename: string | undefined = undefin
     document.body.removeChild(link);
     window.URL.revokeObjectURL(url);
 }
+
+/**
+ * 文件流下载
+ * @param res
+ */
+export function downloadStreamFile(res: any) {
+    const contentType = res.headers['content-type'];
+    const contentDisposition = res.headers['content-disposition'];
+    const filename = decodeURIComponent(contentDisposition.split('; ')[1].split('=')[1])
+    const blob = res.data instanceof Blob ? res.data : new Blob([res.data], { type: contentType });
+    downloadFile(window.URL.createObjectURL(blob), filename);
+}
