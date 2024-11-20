4b63f2f3e1b2186cfb39793463202405c81e112b
喵你个旺呀 <jason-dom@qq.com>
2024-11-20 20:08:29 +0800

    chore: 增加生成加载动画

--- Web/src/views/system/database/component/genEntity.vue
+++ Web/src/views/system/database/component/genEntity.vue
@@ -7,7 +7,7 @@
 					<span> 生成实体 </span>
 				</div>
 			</template>
-			<el-form :model="state.ruleForm" ref="ruleFormRef" label-width="auto">
+			<el-form :model="state.ruleForm" ref="ruleFormRef" label-width="auto" v-loading="state.loading">
 				<el-row :gutter="35">
 					<el-col :xs="24" :sm="24" :md="24" :lg="24" :xl="24" class="mb20">
 						<el-form-item label="表名" prop="tableName" :rules="[{ required: true, message: '表名不能为空', trigger: 'blur' }]">
@@ -38,8 +38,8 @@
 			</el-form>
 			<template #footer>
 				<span class="dialog-footer">
-					<el-button @click="cancel">取 消</el-button>
-					<el-button type="primary" v-reclick="3000" @click="submit">确 定</el-button>
+					<el-button @click="cancel" :disabled="state.loading">取 消</el-button>
+					<el-button type="primary" v-reclick="3000" @click="submit" :disabled="state.loading">确 定</el-button>
 				</span>
 			</template>
 		</el-dialog>
@@ -48,6 +48,7 @@
 
 <script lang="ts" setup name="sysGenEntity">
 import { reactive, ref } from 'vue';
+import { ElMessage } from "element-plus";
 import { camelCase, upperFirst } from 'lodash-es';
 import {useUserInfo} from "/@/stores/userInfo";
 import { getAPI } from '/@/utils/axios-utils';
@@ -58,7 +59,8 @@ const emits = defineEmits(['handleQueryColumn']);
 const ruleFormRef = ref();
 const state = reactive({
 	isShowDialog: false,
-	ruleForm: {} as any
+	ruleForm: {} as any,
+  loading: false
 });
 
 const props = defineProps({
@@ -90,8 +92,13 @@ const cancel = () => {
 const submit = () => {
 	ruleFormRef.value.validate(async (valid: boolean) => {
 		if (!valid) return;
-		await getAPI(SysDatabaseApi).apiSysDatabaseCreateEntityPost(state.ruleForm);
-		closeDialog();
+    state.loading = true;
+    try {
+      await getAPI(SysDatabaseApi).apiSysDatabaseCreateEntityPost(state.ruleForm);
+      closeDialog();
+      ElMessage.success('生成成功');
+    } catch (e) { /* empty */ }
+    state.loading = false;
 	});
 };
 
