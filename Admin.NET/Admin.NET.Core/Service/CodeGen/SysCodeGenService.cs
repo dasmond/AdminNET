111948f37a8592687a9679d5eb1acdaf9a752042
喵你个旺呀 <jason-dom@qq.com>
2024-11-25 19:29:11 +0800

    fixup! fix: 修复代码生成的菜单没有关联应用的问题

--- Admin.NET/Admin.NET.Core/Service/CodeGen/SysCodeGenService.cs
+++ Admin.NET/Admin.NET.Core/Service/CodeGen/SysCodeGenService.cs
@@ -458,12 +458,15 @@ public class SysCodeGenService : IDynamicApiController, ITransient
         await DeleteMenuTree(title, pid == 0 ? MenuTypeEnum.Dir : MenuTypeEnum.Menu);
 
         var parentMenuPath = "";
+        var appId = _userManager.AppId;
+        var appMenuList = new List<SysAppMenu>();
         var lowerClassName =  className[..1].ToLower() + className[1..];
         if (pid == 0)
         {
             // 新增目录，并记录Id
             var dirMenu = new SysMenu { Pid=0, Title=title, Type=MenuTypeEnum.Dir, Icon="robot", Path="/" + className.ToLower(), Component="Layout" };
             pid = (await _db.Insertable(dirMenu).ExecuteReturnEntityAsync()).Id;
+            appMenuList.Add(new SysAppMenu { AppId = appId, MenuId = pid });
         }
         else
         {
@@ -474,6 +477,7 @@ public class SysCodeGenService : IDynamicApiController, ITransient
         // 新增菜单，并记录Id
         var rootMenu = new SysMenu { Pid=pid, Title=title, Type=MenuTypeEnum.Menu, Icon=menuIcon, Path=$"{parentMenuPath}/{className.ToLower()}", Component=$"/{pagePath}/{lowerClassName}/index" };
         pid = (await _db.Insertable(rootMenu).ExecuteReturnEntityAsync()).Id;
+        appMenuList.Add(new SysAppMenu { AppId = appId, MenuId = pid });
 
         var orderNo = 100;
         var menuList = new List<SysMenu>
@@ -499,7 +503,8 @@ public class SysCodeGenService : IDynamicApiController, ITransient
         await _db.Insertable(menuList).ExecuteCommandAsync();
         
         // 新增应用菜单关联
-        await _db.Insertable(menuList.Select(u => new SysAppMenu { AppId = _userManager.AppId, MenuId = u.Id }).ToList()).ExecuteCommandAsync();
+        appMenuList.AddRange(menuList.Select(u => new SysAppMenu { AppId = appId, MenuId = u.Id }));
+        await _db.Insertable(appMenuList).ExecuteCommandAsync();
     }
 
     /// <summary>
