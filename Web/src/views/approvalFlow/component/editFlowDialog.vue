f62ceac3509f48c87231aea62d98a5d1a7d1f8af
samisgod <21gh@163.com>
2024-11-29 11:36:35 +0800

    修复路径报错

--- Web/src/views/approvalFlow/component/editFlowDialog.vue
+++ Web/src/views/approvalFlow/component/editFlowDialog.vue
@@ -38,7 +38,7 @@ import { ElMessageBox } from 'element-plus';
 
 import LogicFlow from '@logicflow/core';
 import { BpmnElement, InsertNodeInPolyline, Menu, MiniMap, SelectionSelect, Snapshot } from '@logicflow/extension';
-import '@logicflow/core/dist/style/index.css';
+import '@logicflow/core/dist/index.css';
 import '@logicflow/extension/lib/style/index.css';
 
 import RegisterEdge from './LogicFlow/Register/RegisterEdge';
