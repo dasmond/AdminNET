df39728f1b547eb0b2e36f9f20e09e9746393976
zuohuaijun <zuohuaijun@163.com>
2024-12-17 10:01:57 +0800

    😎1、调整sql语句打印范围 2、清理代码 3、升级依赖

--- Admin.NET/Admin.NET.Core/Attribute/CommonValidationAttribute.cs
+++ Admin.NET/Admin.NET.Core/Attribute/CommonValidationAttribute.cs
@@ -1,10 +1,10 @@
 ﻿// Admin.NET 项目的版权、商标、专利和其他相关权利均受相应法律法规的保护。使用本项目应遵守相关法律法规和许可证的要求。
-// 
+//
 // 本项目主要遵循 MIT 许可证和 Apache 许可证（版本 2.0）进行分发和使用。许可证位于源代码树根目录中的 LICENSE-MIT 和 LICENSE-APACHE 文件。
-// 
+//
 // 不得利用本项目从事危害国家安全、扰乱社会秩序、侵犯他人合法权益等法律法规禁止的活动！任何基于本项目二次开发而产生的一切法律纠纷和责任，我们不承担任何责任！
 
-namespace Admin.NET.Core.Common;
+namespace Admin.NET.Core;
 
 /// <summary>
 /// 通用接口参数验证特性类
@@ -14,7 +14,7 @@ public class CommonValidationAttribute : ValidationAttribute
 {
     private readonly Dictionary<string, string> _conditions;
     private static readonly Dictionary<string, Delegate> CompiledConditions = new();
-    
+
     /// <summary>
     /// </summary>
     /// <param name="conditionPairs">条件对参数，长度必须为偶数<br/>
@@ -25,10 +25,10 @@ public class CommonValidationAttribute : ValidationAttribute
     /// <code lang="C">
     /// public class ModelInput {
     ///
-    /// 
+    ///
     ///     public string A { get; set; }
     ///
-    /// 
+    ///
     ///     [CommonValidation(
     ///         "A == 1 <value>&amp;&amp;</value> B == null", "当 A == 1时，B不能为空",
     ///         "C == 2 <value>&amp;&amp;</value> B == null", "当 C == 2时，B不能为空"
@@ -50,7 +50,6 @@ public class CommonValidationAttribute : ValidationAttribute
 
     protected override ValidationResult IsValid(object value, ValidationContext validationContext)
     {
-        
         foreach (var (expr, errorMessage) in _conditions)
         {
             var conditionKey = $"{validationContext.ObjectType.FullName}.{expr}";
@@ -59,7 +58,7 @@ public class CommonValidationAttribute : ValidationAttribute
                 condition = CreateCondition(validationContext.ObjectType, expr);
                 CompiledConditions[conditionKey] = condition;
             }
-            
+
             if ((bool)condition.DynamicInvoke(validationContext.ObjectInstance)!)
             {
                 return new ValidationResult(errorMessage ?? $"[{validationContext.MemberName}]校验失败");
@@ -75,7 +74,7 @@ public class CommonValidationAttribute : ValidationAttribute
         {
             // 创建参数表达式
             var parameter = Expression.Parameter(typeof(object), "x");
-            
+
             // 构建 Lambda 表达式
             var lambda = DynamicExpressionParser.ParseLambda(new[] { Expression.Parameter(modelType, "x") }, typeof(bool), expression);
 
